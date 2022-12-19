fe4094bc3b1f80eaf13dba6889d7c4b235b64cfe
EduardMacianHerrera <emacianherrera.cf@iesesteveterradas.cat>
2022-12-19 17:00:16 +0100

    Se abre la galeria y carga la foto. Se abre la camara y se hace la foto pero no se muestra

--- app/src/main/java/com/example/capturaappexterna/MainActivity.java
+++ app/src/main/java/com/example/capturaappexterna/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.capturaappexterna;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -17,8 +19,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonGallery;
+    private Button buttonCamera;
     private ActivityResultLauncher<Intent> someActivityResultLauncher;
-
+    private ActivityResultLauncher<Intent> CameraActivityResultLauncher;
+    private ImageView imageView;
 
     public static int RC_PHOTO_PICKER = 0;
     @Override
@@ -26,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonGallery =  findViewById(R.id.Gallery);
+        buttonCamera = findViewById(R.id.buttonCamera);
 
-        ActivityResultLauncher<Intent> someActivityResultLauncher;
         someActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
@@ -37,8 +41,22 @@ public class MainActivity extends AppCompatActivity {
                             // There are no request codes
                             Intent data = result.getData();
                             Uri uri = data.getData();
-                            ImageView imageView = findViewById(R.id.imageView);
+                            imageView = findViewById(R.id.imageView);
                             imageView.setImageURI(uri);
+                        }
+                    }
+                });
+
+        CameraActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Bundle extras = result.getData().getExtras();
+                            Bitmap imageBitmap = (Bitmap) extras.get("data");
+                            System.out.println("AAAAAAAA");
+                            imageView.setImageBitmap(imageBitmap);
                         }
                     }
                 });
@@ -46,18 +64,39 @@ public class MainActivity extends AppCompatActivity {
         buttonGallery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openSomeActivityForResult(null);
+                openGallery(null);
+            }
+
+
+
+        });
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera(null);
             }
         });
     }
 
-    public void openSomeActivityForResult(View view) {
+
+
+    public void openGallery(View view) {
         //Create Intent
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
         intent.setType("image/jpg");
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         //Launch activity to get result
         someActivityResultLauncher.launch(intent);
+    }
+
+    public void openCamera(View view){
+        //Create Intent
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //intent.setType("image/jpg");
+        intent.putExtra(MediaStore.ACTION_IMAGE_CAPTURE, true);
+        //Launch activity to get result
+        CameraActivityResultLauncher.launch(intent);
     }
 
 }
